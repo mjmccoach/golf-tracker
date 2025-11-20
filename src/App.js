202bd3bb580614a21b50c4abf1f887833a83804c
Matthew <m.mccoach88@gmail.com>
2025-11-20 15:23:37 +0000

    Add clubs to render.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Clubs from './components/clubs.js';
-import Courses from './components/clubs.js';
+import Courses from './components/courses.js';
 
 
 function App() {
   return (
     <div className="App">
       <Clubs/>
+      <Courses/>
     </div>
   );
 }
--- src/components/clubs.js
+++ src/components/clubs.js
@@ -1,9 +1,32 @@
 import React from "react";
 
 const Clubs = () => {
+     const TaylorMadeM2 = "TaylorMade M2";
+     const PingG440 = "Ping G440";
+     const Titleist = "Titleist"
+     const clubs = [
+            {"clubName": "Driver", "brand": TaylorMadeM2, "yardage": 240},
+            {"clubName": "3 Wood", "brand": TaylorMadeM2, "yardage": 220},
+            {"clubName": "4 Iron", "brand": PingG440, "yardage": 180},
+            {"clubName": "5 Iron", "brand": PingG440, "yardage": 170},
+            {"clubName": "6 Iron", "brand": PingG440, "yardage": 160},
+            {"clubName": "7 Iron", "brand": PingG440, "yardage": 150},
+            {"clubName": "8 Iron", "brand": PingG440, "yardage": 140},
+            {"clubName": "9 Iron", "brand": PingG440, "yardage": 130},
+            {"clubName": "Pitching Wedge", "brand": PingG440, "yardage": 115},
+            {"clubName": "50 Wedge", "brand": Titleist, "yardage": 100},
+            {"clubName": "54 Wedge", "brand": Titleist, "yardage": 100},
+            {"clubName": "58 Wedge", "brand": Titleist, "yardage": 100},
+            {"clubName": "Putter", "brand": Titleist, "yardage": 30},
+        ];
 
     return (
-        <div>Clubs</div>
+        <>
+        {clubs.map((club) => 
+        <div>
+            <span>{club.clubName} * {club.brand} * {club.yardage}</span>
+        </div>)}
+        </>
     );
 }
 
